95e0d663d3516b8dba3083609e219f46ffcaf42a
Gabriel Alves <sousa.alves@aluno.uece.br>
2018-01-30 16:53:04 -0300

    correção de cores e layout

---
+++ js/ampliar_imagem.js
@@ -0,0 +1,11 @@
+("#imgportfolio").bind('mouseover',function(){
+    
+	   $(this).animate({height:"200px",width:"200px"});
+   
+});
+
+("#imgportfolio").bind('mouseout',function(){
+    
+       $(this).animate({height:"76px",width:"75px"});
+      
+});
